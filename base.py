b2265b2be583d3df84e9e764797f4e75d8d06500
Francois Guibert <frozax@frozax.com>
2016-06-25 00:50:17 +0200

    base file

---
+++ base.py
@@ -0,0 +1,22 @@
+import sys
+import math
+
+########################################################## HELPER FUNCTIONS
+from collections import namedtuple
+
+def dbg(*args):
+    print("DBG:", *args, file=sys.stderr)
+    
+def dbg_var(*args):
+    for var_name in args:
+        assert type(var_name) == str
+        calling_frame = sys._getframe().f_back
+        var_val = calling_frame.f_locals.get(var_name, calling_frame.f_globals.get(var_name, None))
+        dbg(var_name + '=' + str(var_val))
+
+def french_str_to_float(string):
+    return float(string.replace(",", "."))
+
+
+########################################################## HELPER FUNCTIONS
+
