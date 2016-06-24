c89c8a2d74193608ceb388e45b21ab87147441c7
Francois Guibert <frozax@frozax.com>
2016-06-25 00:14:06 +0200

    initial

---
+++ defibrillateur.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+lon = input()
+lat = input()
+n = int(input())
+for i in range(n):
+    defib = input()
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+print("answer")
