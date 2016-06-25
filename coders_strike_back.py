8c16322e85ec0ce791d710e48a5e95b471abbd74
Francois Guibert <frozax@frozax.com>
2016-06-25 10:09:32 +0200

    coders strike back

---
+++ coders_strike_back.py
@@ -0,0 +1,41 @@
+import sys
+import math
+
+########################################################## HELPER FUNCTIONS
+from collections import namedtuple
+
+def dbg(*args):
+    print("DBG:", *args, file=sys.stderr)
+    
+def dbg_var(*args):
+    for var_name in args:
+        assert type(var_name) == str
+        calling_frame = sys._getframe().f_back
+        var_val = calling_frame.f_locals.get(var_name, calling_frame.f_globals.get(var_name, None))
+        dbg(var_name + '=' + str(var_val))
+
+def french_str_to_float(string):
+    return float(string.replace(",", "."))
+
+
+########################################################## HELPER FUNCTIONS
+
+# game loop
+while True:
+    # next_checkpoint_x: x position of the next check point
+    # next_checkpoint_y: y position of the next check point
+    # next_checkpoint_dist: distance to the next checkpoint
+    # next_checkpoint_angle: angle between your pod orientation and the direction of the next checkpoint
+    x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
+    opponent_x, opponent_y = [int(i) for i in input().split()]
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+
+
+    # You have to output the target position
+    # followed by the power (0 <= thrust <= 100)
+    # i.e.: "x y thrust"
+    print(str(next_checkpoint_x) + " " + str(next_checkpoint_y) + " 80")
+
+
